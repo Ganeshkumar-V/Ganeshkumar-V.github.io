54354b40fd571784b24fb05a832c112fc76b9c62
Ganeshkumar-V <ganeshkv@iitgn.ac.in>
2025-04-23 00:21:13 +0530

    Create node js app

---
+++ portfolio/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Hi, I'm Ganeshkumar</h1>
+      <p>PhD Researcher at IITGN | Rocketry & Fluid Dynamics Enthusiast</p>
+    </div>
+  );
+}
+
+export default App;
+
